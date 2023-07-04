f03fe607440566d82fbb73205194c33267f8aa62
Maulana Malik Y <67380302+maulanamaleek@users.noreply.github.com>
2023-07-05 00:52:50 +0700

    fix: bug from self test

--- components/OrderSuccess/index.tsx
+++ components/OrderSuccess/index.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 import React from 'react';
 
+const classes = {
+  title: "w-full h-full fixed top-0 left-0 flex flex-col items-center justify-center gap-5",
+  btn: "py-2 text-white bg-orange-600 px-5 rounded-full",
+};
+
 const OrderSuccess = () => {
   return (
-    <div className="w-full h-full fixed top-0 left-0 flex flex-col items-center justify-center gap-5">
+    <div className={classes.title}>
       <h1>Your Order Has Been Created!</h1>
       <Link href="/">
-        <button className="py-2 text-white bg-orange-600 px-5 rounded-full">Back to Home</button>
+        <button className={classes.btn}>Back to Home</button>
 
       </Link>
     </div>
